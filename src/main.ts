f16599b65f7df618491465a030086220f6aeb43e
andrewuskov <andreuskov2211@yandex.ru>
2026-04-30 18:21:20 +0300

    Nest - Middleware

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { logger } from './common/meddlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.use(logger);
 
   await app.listen(process.env.PORT ?? 3000);
 }
