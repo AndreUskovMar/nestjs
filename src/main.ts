b2cb2af18e71dcbdcbdf4fdbe3f9107d17fb6680
andrewuskov <andreuskov2211@yandex.ru>
2026-06-19 19:23:18 +0300

    Logging

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import { CustomLogger } from './common/logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
 
   const config = app.get(ConfigService);
 
   app.use(cookieParser());
+
+  app.useLogger(new CustomLogger());
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -20,6 +25,11 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'DELETE', 'PATCH'],
     exposedHeaders: ['Content-Disposition', 'Set-Cookie'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Api-Key'],
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
   });
 
   await app.listen(process.env.PORT ?? 3000);
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,20 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression, Interval, Timeout } from '@nestjs/schedule';
 
 @Injectable()
 export class TaskService {
+  private readonly logger = new Logger(TaskService.name);
+
   @Cron(CronExpression.EVERY_30_SECONDS)
   handleCron() {
-    console.log('Running cron job every 30 seconds');
+    this.logger.log('Running cron job every 30 seconds');
   }
 
   @Interval(5000)
   handleInterval() {
-    console.log('Running interval job every 5 seconds');
+    this.logger.log('Running interval job every 5 seconds');
   }
 
   @Timeout(10000)
   handleTimeout() {
-    console.log('Running timeout job after 10 seconds');
+    this.logger.log('Running timeout job after 10 seconds');
   }
 }
