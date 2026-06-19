9fffae00ab0e21fc49beda74880180484f43b314
andrewuskov <andreuskov2211@yandex.ru>
2026-06-19 18:16:20 +0300

    Cors

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = app.get(ConfigService);
+
   app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGINS').split(','),
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    exposedHeaders: ['Content-Disposition', 'Set-Cookie'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Api-Key'],
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
